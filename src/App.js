e37a42d9c3480a95e2be3534a89966d82f72b6ab
PJMantoss <joelptoss@gmail.com>
2019-09-28 18:51:55 +0100

    Refactored the Modal component

--- src/App.js
+++ src/App.js
@@ -17,33 +17,31 @@ function Input(props){
 		);
 };
 
-var Modal = React.createClass({
-	render: function() {
-		return (
-			<div className="Modal">
-				<form 
-					onSubmit={this.props.onSubmit}
-					className="ModalForm">
-					<Input
-						id="name"
-						type="text"
-						placeholder="Jack-Edward Oliver" />
-					<Input
-						id="username"
-						type="email"
-						placeholder="mrjackolai@gmail.com" />
-					<Input
-						id="password"
-						type="password"
-						placeholder="password" />
-					<button>
-						Log in <i className="fa fa-fw fa-chevron-right"></i>
-					</button>
-				</form>
-			</div>
-		);
-	}
-});
+function Modal(props){
+  return (
+    <div className="Modal">
+      <form 
+        onSubmit={this.props.onSubmit}
+        className="ModalForm">
+        <Input
+          id="name"
+          type="text"
+          placeholder="Jack-Edward Oliver" />
+        <Input
+          id="username"
+          type="email"
+          placeholder="mrjackolai@gmail.com" />
+        <Input
+          id="password"
+          type="password"
+          placeholder="password" />
+        <button>
+          Log in <i className="fa fa-fw fa-chevron-right"></i>
+        </button>
+      </form>
+    </div>
+  );
+}
 
 function App() {
   return (
