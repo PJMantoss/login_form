e9dd7dad29ff589807be321d7d57ad9348f42d4b
PJMantoss <joelptoss@gmail.com>
2019-09-28 18:48:40 +0100

    Add the Input and Modal functions to App

--- src/App.js
+++ src/App.js
@@ -2,6 +2,49 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function Input(props){
+		return (
+			<div className="Input">
+				<input 
+					id={this.props.id}
+					autoComplete="false"
+					required
+					type={this.props.type}
+					placeholder={this.props.placeholder}
+				/>	
+				<label htmlFor={this.props.id}></label>
+			</div>
+		);
+};
+
+var Modal = React.createClass({
+	render: function() {
+		return (
+			<div className="Modal">
+				<form 
+					onSubmit={this.props.onSubmit}
+					className="ModalForm">
+					<Input
+						id="name"
+						type="text"
+						placeholder="Jack-Edward Oliver" />
+					<Input
+						id="username"
+						type="email"
+						placeholder="mrjackolai@gmail.com" />
+					<Input
+						id="password"
+						type="password"
+						placeholder="password" />
+					<button>
+						Log in <i className="fa fa-fw fa-chevron-right"></i>
+					</button>
+				</form>
+			</div>
+		);
+	}
+});
+
 function App() {
   return (
     <div className="App">
