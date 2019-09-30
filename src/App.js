c8a4c59a8e815ac683f90aa9a22164a7a7e64530
PJMantoss <joelptoss@gmail.com>
2019-09-30 14:10:06 +0100

    Update Input component Codes

--- src/App.js
+++ src/App.js
@@ -3,85 +3,95 @@ import './App.css';
 import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 
 function Input(props){
+		  return (
+			  <div className="Input">
+				  <input 
+					  id={props.id}
+					  autoComplete="false"
+					  required
+					  type={props.type}
+					  placeholder={props.placeholder}
+				  />	
+				  <label htmlFor={props.id}></label>
+			  </div>
+		  );
+  };
+  
+  class Modal extends React.Component {
+	  constructor(props){
+		  super(props)
+		  this.state = {
+			  message: "",
+		  }
+	  }
+
+	  render(){
 		return (
-			<div className="Input">
-				<input 
-					id={this.props.id}
-					autoComplete="false"
-					required
-					type={this.props.type}
-					placeholder={this.props.placeholder}
-				/>	
-				<label htmlFor={this.props.id}></label>
+			<div className="Modal">
+			  <form 
+				onSubmit={this.props.onSubmit}
+				className="ModalForm">
+				<Input
+				  id="name"
+				  type="text"
+				  placeholder="PJ Mantoss" />
+				<Input
+				  id="username"
+				  type="email"
+				  placeholder="joelptoss@gmail.com" />
+				<Input
+				  id="password"
+				  type="password"
+				  placeholder="password" />
+				<button>
+				  Log in <i className="fa fa-fw fa-chevron-right"></i>
+				</button>
+			  </form>
 			</div>
-		);
-};
-
-function Modal(props){
-  return (
-    <div className="Modal">
-      <form 
-        className="ModalForm">
-        <Input
-          id="name"
-          type="text"
-          placeholder="Jack-Edward Oliver" />
-        <Input
-          id="username"
-          type="email"
-          placeholder="mrjackolai@gmail.com" />
-        <Input
-          id="password"
-          type="password"
-          placeholder="password" />
-        <button>
-          Log in <i className="fa fa-fw fa-chevron-right"></i>
-        </button>
-      </form>
-    </div>
-  );
-}
-
-class App extends React.Component {
-  constructor(){
-    super()
-    this.state = {
-      mounted: false
-    }
-    this.handleSubmit = this.handleSubmit.bind(this)
+		  );
+	  }
   }
-
-  componentDidMount(){
-    this.setState({
-      mounted: true
-    });
+  
+  class App extends React.Component {
+	constructor(props){
+	  super(props)
+	  this.state = {
+		mounted: false
+	  }
+	  this.handleSubmit = this.handleSubmit.bind(this)
+	}
+  
+	componentDidMount(){
+	  this.setState({
+		mounted: true
+	  });
+	}
+  
+	handleSubmit(e){
+	  e.preventDefault();
+	  this.setState({
+		mounted: false
+	  });
+	}
+  
+	render(){
+	  let child = ""
+  
+	  if (this.state.mounted){
+		child = (<Modal onSubmit={this.handleSubmit} />)
+	  }
+	  return (
+		<div className="App">
+		   <ReactCSSTransitionGroup 
+					  transitionName="example"
+					  transitionEnterTimeout={500}
+					  transitionLeaveTimeout={300}
+		   >
+						  {child}
+				  </ReactCSSTransitionGroup>
+		</div>
+	  );
+	}
   }
-
-  handleSubmit(e){
-    e.preventDefault();
-    this.setState({
-      mounted: false
-    });
-  }
-
-  render(){
-    let child = ""
-
-    if (this.state.mounted){
-      child = (<Modal onSubmit={this.handleSubmit} />)
-    }
-    return (
-      <div className="App">
-         <ReactCSSTransitionGroup 
-					transitionName="example"
-					transitionEnterTimeout={500}
-					transitionLeaveTimeout={300}
-         >
-						{child}
-				</ReactCSSTransitionGroup>
-      </div>
-    );
-  }
-}
-
-export default App;
+  
+  export default App;
